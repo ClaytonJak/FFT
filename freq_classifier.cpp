b0c1adb96c2ed1e99f1d86c6040d88d19e9dfd99
Clayton Jaksha <claytonjak@gmail.com>
2022-01-11 09:55:18 +0900

    changes to freq classifier to even print lengths. test signal added freq.

--- freq_classifier.cpp
+++ freq_classifier.cpp
@@ -6,13 +6,29 @@ std::string freq_classify(double in_freq) {
         out_string = std::to_string(in_freq / 1000000000.);
         out_string.append(" GHz");
     }
-    else if (in_freq >= 1000000.) {
+    else if (in_freq >= 10000000.) {
         out_string = std::to_string(in_freq / 1000000.);
         out_string.append(" MHz");
     }
-    else if (in_freq >= 1000.) {
+    else if (in_freq >= 10000000.) {
+        out_string = std::to_string(in_freq / 1000000.);
+        out_string.append("  MHz");
+    }
+    else if (in_freq >= 1000000.) {
+        out_string = std::to_string(in_freq / 1000000.);
+        out_string.append("   MHz");
+    }
+    else if (in_freq >= 100000.) {
         out_string = std::to_string(in_freq / 1000.);
         out_string.append(" kHz");
+    }
+    else if (in_freq >= 10000.) {
+        out_string = std::to_string(in_freq / 1000.);
+        out_string.append("  kHz");
+    }
+    else if (in_freq >= 1000.) {
+        out_string = std::to_string(in_freq / 1000.);
+        out_string.append("   kHz");
     }
     else {
         out_string = std::to_string(in_freq);
--- seed_test_signal.cpp
+++ seed_test_signal.cpp
@@ -5,15 +5,17 @@ CArray seed_test_signal() { //sample rate is measured in Hz
     Complex test_sig[N];
     const int w_n1 = 440;
     const int w_n2 = 500;
+    const int w_n3 = 1200;
     const float A_n1 = 10.0;
-    const float A_n2 = 1.5;
+    const float A_n2 = 3.5;
+    const float A_n3 = 0.8;
     // analog time signal is cos(w_n1*2*pi*t)+0.7*cos(w_n2*2*pi*t)
 
     for (int i = 0; i < N; ++i) {
         
         // sample at sample rate
         double t = i * 2 * pi / sample_rate;
-        test_sig[i] = A_n1*cos(t*w_n1)+A_n2*cos(t*w_n2);
+        test_sig[i] = A_n1*cos(t*w_n1)+A_n2*cos(t*w_n2)+A_n3*sin(t*w_n3);
         //std::cout << test_sig[i] << std::endl; //use this for debugging
     }
     CArray test_sig_array(test_sig,N);
